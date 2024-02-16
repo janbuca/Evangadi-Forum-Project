301670d7a1789ce8c0ec3f2a9229207cb1a1ba37
janbuca <janbuca333@gmail.com>
2024-02-16 04:58:38 -0800

    All Route can be create

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const app = express();
 const port = 5500;
 
-const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-app.listen(port, (err) => {
-	if (err) {
-		console.log(err.message);
-	} else {
-		console.log(`listen on ${port}`);
-	}
- });
+// Register route
+app.post("/api/users/register", (req, res) => {
+    // Code to register a user goes here...
+    res.send("Registering new user");
+});
+
+//Login user 
+app.post("/api/users/login", (req, res) => {
+    // Code to register a user goes here...
+    res.send("Registering new user");
+});
+
+//Check user
+app.get("/api/users/check", (req, res) => {
+    // Code to register a user goes here...
+    res.send("Check new user");
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
