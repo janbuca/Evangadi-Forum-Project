65f4cc8e688ef7d7fa85e182c8740ae3485aa57e
janbuca <janbuca333@gmail.com>
2024-02-20 02:40:33 -0800

    app.js will be up dateing

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-
-
-// const express = require('express');
+ const express = require('express');
 // const userRouter = require('./routes/userRoutes');
-// const app = express();
+ const app = express();
+const port = 5500
+
+ app.listen(port, (err)=> {
+    if (err) {
+        console.log(err.message);
+    }else{
+        console.log( `Server is running on ${port}` );
+    }
+ })
+   
 // //const userRouter = require('./routes/userRoutes');
 
 // app.get('/', (req, res) => {
