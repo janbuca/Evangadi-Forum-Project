4133901e34f902cafd70f8b8184d77e2163b6352
janbuca <janbuca333@gmail.com>
2024-02-20 05:10:43 -0800

    the app.js can be update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
  const app = express();
 const port = 5500;
 
+// db connection
+const dbconnection = require("./db/dbConfig");
+
 // use routes middleware file
 //const userRoutes = require("./routes/userRoute");
 const userRoutes = require("./routes/userRoute")
@@ -9,18 +12,32 @@ const userRoutes = require("./routes/userRoute")
 // questions routes middleware file
 // answer route middleware file 
 
+async function start() {
+	try {
+		const result = await dbconnection.execute("select 'test'");
+
+		await app.listen(port);
+		console.log("database connection established");
+		console.log(`listing on ${port}`);
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+start();
+
 
 app.use("/api/users", userRoutes)
 
 
 
- app.listen(port, (err)=> {
-    if (err) {
-        console.log(err.message);
-    }else{
-        console.log( `Server is running on ${port}` );
-    }
- }) 
+//  app.listen(port, (err)=> {
+//     if (err) {
+//         console.log(err.message);
+//     }else{
+//         console.log( `Server is running on ${port}` );
+//     }
+//  }) 
 
 
 
