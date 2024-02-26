8f7caa6797410511090b1de8bb555411274d9300
janbuca <janbuca333@gmail.com>
2024-02-26 09:52:53 -0800

    The front end registration part can be started

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <section>
+      <div>
+        <span>username :--</span>
+        <input type="text" placeholder='username' />
+      </div>
+      <div>
+        <span>First name :--</span>
+        <input type="text" placeholder='first name' />
+      </div>
+      <div>
+        <span>last name :--</span>
+      </div>
+      <div></div>
+      <div></div>
+    </section>
   )
 }
 
