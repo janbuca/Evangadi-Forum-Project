38cb6d8cf3f05d26ce603628e58ed991bba0037e
janbuca <janbuca333@gmail.com>
2024-02-26 12:26:51 -0800

    helemt can be add

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
- require("dotenv").config()
- const express = require('express');
- const app = express();
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const port = 5500;
 
-const cors = require('cors')
-app.use(cors())
+// const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+
+const cors = require("cors");
+app.use(cors());
 
 // parse requests of content-type - application/json
 
@@ -13,32 +16,29 @@ const dbconnection = require("./db/dbConfig");
 
 // use routes middleware file
 //const userRoutes = require("./routes/userRoute");
-const userRoutes = require("./routes/userRoute")
+const userRoutes = require("./routes/userRoute");
 
 // questions routes middleware file
-// answer route middleware file 
+// answer route middleware file
 
 // json middleware to extract json data
 app.use(express.json());
 
 async function start() {
-	try {
-		const result = await dbconnection.execute("select 'test'");
+  try {
+    const result = await dbconnection.execute("select 'test'");
 
-		await app.listen(port);
-		console.log("database connection established");
-		console.log(`listing on ${port}`);
-	} catch (error) {
-		console.log(error.message);
-	}
+    await app.listen(port);
+    console.log("database connection established");
+    console.log(`listing on ${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 start();
 
-
-app.use("/api/users", userRoutes)
-
-
+app.use("/api/users", userRoutes);
 
 //  app.listen(port, (err)=> {
 //     if (err) {
@@ -46,23 +46,13 @@ app.use("/api/users", userRoutes)
 //     }else{
 //         console.log( `Server is running on ${port}` );
 //     }
-//  }) 
+//  })
 
-
-
-
-
-   
 // //const userRouter = require('./routes/userRoutes');
-
-
 
 // // User route middleware file
 
-
-
 // app.use("api/users", userRouter);
-
 
 // app.listen(3000, () => {
 //     console.log('Server is running on port 3000');
--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router =express.Router()
-const {postanswer, getanswer} = require("../controller/answerController")
+const {postanswer, getanswer} = require("../controllers/answerController")
 
 
 
